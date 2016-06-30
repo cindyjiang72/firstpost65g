8e20ce45445200e3967be22647b1d37f6e674507
cindyjiang72 <yujiang17@cushing.org>
2016-06-29 22:40:55 -0400

    Lecture4

---
+++ Lecture4/Lecture4/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Lecture4
+//
+//  Created by JiangYuan on 6/29/16.
+//  Copyright © 2016 JiangYuan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var myButton: UIView!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
