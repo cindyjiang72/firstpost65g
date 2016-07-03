64b7f1e291627427a7fa419d566f8ba6eaa7dc83
cindyjiang72 <yujiang17@cushing.org>
2016-07-03 13:43:13 -0400

    updated on assignment2 7.3

--- Assignment2/Assignment2/Assignment2/MainController.swift
+++ Assignment2/Assignment2/Assignment2/MainController.swift
@@ -17,9 +17,82 @@ class MainController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
 
+class Problem2ViewController: UIViewController {
+    @IBOutlet weak var ShowText: UITextView!
+    @IBAction func Hit(sender: AnyObject) {
+         ShowText.text = "Hi there!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Problem 2"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    
+    
+}
+
+class Problem3ViewController: UIViewController {
+    
+    @IBOutlet weak var myText: UITextView!
+
+    @IBAction func myButton(sender: AnyObject) {
+        myText.text = "Hi there!"
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Problem 3"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+ 
+    
+    
+}
+
+class Problem4ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func hitButton(sender: AnyObject) {
+        textView.text = "Hi there!"
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Problem 4"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
